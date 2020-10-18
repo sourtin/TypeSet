44ed4e1ca61421d842c114b9bca3bbee812e7f85
William Earley <nitrous@sourt.in>
2020-10-18 10:02:25 +0100

    countable: accurate type constraints, ensured instances all dense, tidy up code a bit

--- src/TypeSet.hs
+++ src/TypeSet.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE TupleSections #-}
 
 module TypeSet where
 import Data.Proxy (Proxy(Proxy),asProxyTypeOf)
@@ -11,7 +12,9 @@ import Numeric.Natural (Natural)
 import Data.Void (Void)
 import Data.Bits (Bits(bitSizeMaybe))
 import Data.Foldable (foldl')
-import Data.List (tails)
+import Data.List (tails, foldl1')
+import Data.Maybe (catMaybes)
+import Data.Tuple (swap)
 import Control.Applicative (liftA2)
 import qualified Data.Map.Strict as MS
 
@@ -108,39 +111,41 @@ class Countable a where
   fromNatural' :: Natural -> a
   fromNatural' = maybe (error "invalid domain") id . fromNatural
   enumerate :: [(Natural, a)]
+  enumerate = 
+    catMaybes . map (\x -> (x,) <$> fromNatural x) $
+    case upperBound (Proxy :: Proxy a) of
+      Just 0 -> []
+      Just p -> [0..p-1]
+      Nothing -> [0..]
   upperBound :: Proxy a -> Maybe Natural
-  dense :: Proxy a -> Bool
-  dense Proxy = False
+class Countable a => Finite a where
+  upperBound' :: Proxy a -> Natural
+  upperBound' = maybe undefined id . upperBound
 
+instance Finite Void
 instance Countable Void where
-  toNatural = \case {}
+  toNatural = \case
   fromNatural = const Nothing
-  enumerate = []
   upperBound Proxy = Just 0
-  dense Proxy = True
+instance Finite ()
 instance Countable () where
   toNatural () = 0
   fromNatural 0 = Just ()
   fromNatural _ = Nothing
-  enumerate = [(0, ())]
   upperBound Proxy = Just 1
-  dense Proxy = True
+instance Finite Bool
 instance Countable Bool where
   toNatural False = 0
   toNatural True = 1
   fromNatural 0 = Just False
   fromNatural 1 = Just True
   fromNatural _ = Nothing
-  enumerate = [(0, False), (1, True)]
   upperBound Proxy = Just 2
-  dense Proxy = True
 instance Countable Natural where
   toNatural = id
   fromNatural = Just
   fromNatural' = id
-  enumerate = [(x,x) | x <- [0..]]
   upperBound Proxy = Nothing
-  dense Proxy = True
 instance Countable Integer where
   toNatural x | x >= 0 = 2 * fromInteger x
               | x < 0  = 2 * fromInteger (-x) - 1
@@ -148,88 +153,78 @@ instance Countable Integer where
                      (x, 0) -> x
                      (x, 1) -> -1-x
   fromNatural = Just . fromNatural'
-  enumerate = [(x,fromNatural' x) | x <- [0..]]
   upperBound Proxy = Nothing
-  dense Proxy = True
 -- instance {-# OVERLAPPABLE #-} Bits b => Countable b where
+instance (Finite a, Finite b) => Finite (Either a b)
 instance (Countable a, Countable b) => Countable (Either a b) where
-  toNatural (Left a) = 2 * toNatural a
-  toNatural (Right b) = 1 + 2 * toNatural b
-  fromNatural x = case divMod x 2 of
-                    (x, 0) -> Left <$> fromNatural x
-                    (x, 1) -> Right <$> fromNatural x
-  enumerate = go enumerate enumerate
-    where go ((x,u):xs) ((y,v):ys) = (2*x,Left u) : (2*y+1,Right v) : go xs ys
-          go ((x,u):xs) [] = (2*x,Left u) : go xs []
-          go [] ((y,v):ys) = (2*y+1,Right v) : go [] ys
-          go [] [] = []
-  upperBound Proxy = do l <- upperBound (Proxy :: Proxy a)
-                        r <- upperBound (Proxy :: Proxy b)
-                        return $ max (2 * l + 1) (2 * r)
-  dense Proxy =
-    dense (Proxy :: Proxy a) && dense (Proxy :: Proxy b) &&
-    (upperBound (Proxy :: Proxy a) == upperBound (Proxy :: Proxy b) ||
-     upperBound (Proxy :: Proxy a) == (succ <$> upperBound (Proxy :: Proxy b)))
+  toNatural x = case (upperBound (Proxy :: Proxy a), upperBound (Proxy :: Proxy b), x) of
+    (Just a, _, Left y) -> toNatural y
+    (Just a, _, Right y) -> a + toNatural y
+    (Nothing, Just b, Left y) -> b + toNatural y
+    (Nothing, Nothing, Left y) -> 2 * toNatural y
+    (Nothing, Nothing, Right y) -> 1 + 2 * toNatural y
+  fromNatural x = case (upperBound (Proxy :: Proxy a), upperBound (Proxy :: Proxy b)) of
+    (Just a, _) -> if x < a then Left <$> fromNatural x else Right <$> fromNatural (x - a)
+    (Nothing, Just b) -> if x < b then Right <$> fromNatural x else Left <$> fromNatural (x - b)
+    (Nothing, Nothing) -> case divMod x 2 of
+                            (x, 0) -> Left <$> fromNatural x
+                            (x, 1) -> Right <$> fromNatural x
+  upperBound Proxy = liftA2 (+) (upperBound (Proxy :: Proxy a))
+                                (upperBound (Proxy :: Proxy b))
+instance (Finite a, Finite b) => Finite (a, b)
 instance (Countable a, Countable b) => Countable (a, b) where
-  toNatural (a, b) = let x = toNatural a
-                         y = toNatural b
-                     in ((x + y) * (x + y + 1)) `div` 2 + y
-  fromNatural x = do let (y,z) = cantorUnpair x
-                     a <- fromNatural y
-                     b <- fromNatural z
-                     return (a,b)
-  enumerate = map (\x -> (toNatural x, x)) $ liftA2 (,) (map snd enumerate) (map snd enumerate)
-  upperBound Proxy = toNatural <$> liftA2 (,)
-                        (upperBound (Proxy :: Proxy a))
-                        (upperBound (Proxy :: Proxy b))
-  dense Proxy =
-    dense (Proxy :: Proxy a) && dense (Proxy :: Proxy b) && 
-    upperBound (Proxy :: Proxy a) == Nothing &&
-    upperBound (Proxy :: Proxy b) == Nothing
-    -- it is possible to make this dense for finite a,b by using a pairing function
-    -- based on square numbers rather than triangle numbers
-instance (Countable a, Countable b) => Countable (a -> b) where
-  toNatural f = case upperBound (Proxy :: Proxy b) of
-                  Just p -> sum [p^n * toNatural (f a) | (n,a) <- enumerate]
-  fromNatural x = do p <- upperBound (Proxy :: Proxy b)
-                     let as = enumerate :: [(Natural, a)]
-                     l <- go p (map fst as) 0 (divMod x p)
-                     let m = MS.fromList l
-                     return ((m MS.!) . toNatural)
-    where go :: Natural -> [Natural] -> Natural -> (Natural,Natural) -> Maybe [(Natural, b)]
-          go p (n:ns) n' (x,d)
-            | n' < n && d == 0 = go p (n:ns) (n'+1) (divMod x p)
-            | n' == n = do b <- fromNatural d
-                           rem <- go p ns (n'+1) (divMod x p)
-                           return ((n,b) : rem)
-            | otherwise = Nothing
-          go _ [] _ (0,0) = Just []
-          go _ _ _ _ = Nothing
-  enumerate = case upperBound (Proxy :: Proxy b) of
-                Just p ->
-                  let as = map ((p^) . fst) (enumerate :: [(Natural, a)])
-                      bs = map fst (enumerate :: [(Natural, b)])
-                      n = length as
-                  in map ((\n -> (n, fromNatural' n)) . sum . zipWith (*) as) (cartPow bs n)
-    where cartPow xs 0 = [[]]
-          cartPow xs n = liftA2 (:) xs (cartPow xs (n-1))
+  toNatural (x, y) =
+    let m = toNatural x
+        n = toNatural y
+    in case (upperBound (Proxy :: Proxy a), upperBound (Proxy :: Proxy b)) of
+      (Just a, _) -> n*a + m
+      (Nothing, Just b) -> m*b + n
+      (Nothing, Nothing) -> cantorPair m n
+  fromNatural x = (\(x,y) -> liftA2 (,) (fromNatural x) (fromNatural y)) $
+    case (upperBound (Proxy :: Proxy a), upperBound (Proxy :: Proxy b)) of
+      (Just a, _) -> swap $ divMod x a
+      (Nothing, Just b) -> divMod x b
+      (Nothing, Nothing) -> cantorUnpair x
+  upperBound Proxy = liftA2 (*) (upperBound (Proxy :: Proxy a))
+                                (upperBound (Proxy :: Proxy b))
+instance (Finite a, Finite b) => Finite (a -> b)
+instance (Finite a, Countable b) => Countable (a -> b) where
+  toNatural f = case (upperBound (Proxy :: Proxy a), upperBound (Proxy :: Proxy b)) of
+                  (Just 0, _) -> 0
+                  (_, Just p) -> sum [p^n * toNatural (f a) | (n,a) <- enumerate]
+                  (_, Nothing) -> snd . cantorZip' $ map (toNatural . f . snd) enumerate
+  fromNatural x = 
+    let Just n = upperBound (Proxy :: Proxy a) in
+    case upperBound (Proxy :: Proxy b) of
+      Nothing | n == 0 -> Just (\case)
+              | otherwise -> 
+        let m = MS.fromList . zip [0..n-1] . map fromNatural' $ cantorUnzip' (pred n) x
+        in Just ((m MS.!) . toNatural)
+      Just p ->
+        let m = MS.fromList . zip [0..n-1] . map fromNatural' $ digitsReverse p x ++ repeat 0
+        in if (x < p^n) then Just ((m MS.!) . toNatural) else Nothing
   upperBound Proxy = liftA2 (^) (upperBound (Proxy :: Proxy b))
                                 (upperBound (Proxy :: Proxy a))
-  dense Proxy = dense (Proxy :: Proxy a) && dense (Proxy :: Proxy b)
 instance Countable a => Countable [a] where
-  toNatural xs = case upperBound (Proxy :: Proxy a) of
-                       Just p -> foldl' ((+) . ((p+1)*)) 0 (map (succ . toNatural) xs)
-  fromNatural x = case upperBound (Proxy :: Proxy a) of
-                       Just p -> sequence . map go $ digits (p+1) x []
-    where digits p 0 ds = ds
-          digits p n ds = case divMod n p of (n',d) -> digits p n' (d : ds)
-          go 0 = Nothing
-          go a = fromNatural (a - 1)
-  enumerate = map (\x -> (toNatural x, x)) $ concat enumerate'
-    where xs = map snd enumerate
-          enumerate' = [[]] : [[(y:ys) | y <- xs, ys <- yss] | yss <- enumerate']
+  toNatural = case upperBound (Proxy :: Proxy a) of
+                       Just p -> pred . foldl' ((+) . (p*)) 1 . map toNatural
+                       Nothing -> cantorZip . map toNatural
+  fromNatural = case upperBound (Proxy :: Proxy a) of
+                       Just p -> (>>= sequence . map fromNatural) . validate . digits p . succ
+                       Nothing -> Just . map fromNatural' . cantorUnzip
+    where validate (1:xs) = Just xs
+          validate _      = Nothing
   upperBound Proxy = Nothing
-  dense Proxy = False
+
+digitsReverse :: Integral a => a -> a -> [a]
+digitsReverse p = go
+  where go 0 = []
+        go n = let (n',d) = divMod n p in d : go n'
+
+digits :: Integral a => a -> a -> [a]
+digits p = flip go []
+  where go 0 = id
+        go n = let (n',d) = divMod n p in go n' . (d :)
 
 {- snippet: haskell wiki -}
 (^!) :: Num a => a -> Int -> a
@@ -248,13 +243,29 @@ squareRoot n =
   in  head $ dropWhile (not . isRoot) iters
 {- end snippet; -}
 
-cantorPair :: Integral a => (a,a) -> a
-cantorPair (x, y) = ((x + y) * (x + y + 1)) `div` 2 + y
+cantorPair :: Natural -> Natural -> Natural
+cantorPair x y = ((x + y) * (x + y + 1)) `div` 2 + y
 
-cantorUnpair :: Integral a => a -> (a,a)
+cantorZip' :: [Natural] -> (Natural, Natural)
+cantorZip' [x] = (0, x)
+cantorZip' (x:xs) = let (n, z) = cantorZip' xs in (n+1, cantorPair x z)
+
+cantorZip :: [Natural] -> Natural
+cantorZip [] = 0
+cantorZip xs = succ . uncurry cantorPair $ cantorZip' xs
+
+cantorUnpair :: Natural -> (Natural, Natural)
 cantorUnpair z = let xy1 = fromInteger . squareRoot . toInteger $ 2*z
                      t1 = xy1 * (xy1+1) `div` 2
                      t2 = xy1 * (xy1-1) `div` 2
                      y1 = z - t1
                      y2 = z - t2
                  in if z >= t1 then (xy1-y1,y1) else (xy1-1-y2,y2)
+
+cantorUnzip' :: Natural -> Natural -> [Natural]
+cantorUnzip' 0 x = [x]
+cantorUnzip' n z = let (x, z') = cantorUnpair z in x : cantorUnzip' (n-1) z'
+
+cantorUnzip :: Natural -> [Natural]
+cantorUnzip 0 = []
+cantorUnzip n = uncurry cantorUnzip' . cantorUnpair $ pred n
