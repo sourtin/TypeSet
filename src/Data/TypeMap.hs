f0bbfa3b80d282b080746cf2750ec3d9df316b15
William Earley <nitrous@sourt.in>
2020-10-24 07:36:04 +0100

    instance TypeMapTotal ((->) k) k

--- src/Data/TypeMap.hs
+++ src/Data/TypeMap.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TupleSections #-}
 
 module Data.TypeMap where
 
+import Data.Maybe (catMaybes)
 import Numeric.Natural (Natural)
-import Data.TypeSet.Theory (Countable)
+import Data.TypeSet.Theory (Countable(..), Finite)
+import qualified Data.Map.Strict as MS
 
 infixl 9 !
 (!) :: TypeMapTotal m k => k -> m v -> v
@@ -14,7 +18,8 @@ infixl 9 !?
 (!?) :: TypeMapPartial m k => k -> m v -> Maybe v
 (!?) = Data.TypeMap.lookup
 
-class Countable k => TypeMap m k | m -> k where
+class (Eq k, Finite k) => TypeMap m k | m -> k where
+  lookup :: k -> m v -> Maybe v
   assocs :: m v -> [(k, v)]
   replace :: k -> v -> m v -> m v
   replaceWith :: (v -> v) -> k -> m v -> m v
@@ -22,6 +27,22 @@ class Countable k => TypeMap m k | m -> k where
   mapWithKey :: (k -> a -> b) -> (m a -> m b)
   mapAccum :: (a -> b -> (a, c)) -> a -> (m b -> (a, m c))
   mapAccumWithKey :: (a -> k -> b -> (a, c)) -> a -> (m b -> (a, m c))
+  mapAccumRWithKey :: (a -> k -> b -> (a, c)) -> a -> (m b -> (a, m c))
+  -- | precondition for TypeMapTotal instances (not checked):
+  --   [k] is a permutation of enumerate
+  mapAccumWithKeyBy :: [k] -> (a -> k -> b -> (a, c)) -> a -> (m b -> (a, m c))
+
+  assocs = catMaybes . flip Prelude.map enumerate . go
+    where go m k = (k,) <$> Data.TypeMap.lookup k m
+  replace k = flip replaceWith k . const
+  replaceWith f k = Data.TypeMap.mapWithKey (\k' v -> if k == k' then f v else v)
+  map = mapWithKey . const
+  mapWithKey = (snd .) . flip mapAccumWithKey () . const . ((((),) .) .)
+  mapAccum = mapAccumWithKey . flip . const
+  mapAccumWithKey = mapAccumWithKeyBy enumerate
+  mapAccumRWithKey = mapAccumWithKeyBy (reverse enumerate)
+
+  {-# MINIMAL lookup, mapAccumWithKeyBy #-}
 
 class TypeMap m k => TypeMapTotal m k | m -> k where
   build :: (k -> v) -> m v
@@ -42,7 +63,6 @@ class TypeMap m k => TypeMapPartial m k | m -> k where
   update :: (v -> Maybe v) -> k -> m v -> m v
   updateLookup :: (v -> Maybe v) -> k -> m v -> (Maybe v, m v)
   alter :: (Maybe v -> Maybe v) -> k -> m v -> m v
-  lookup :: k -> m v -> Maybe v
   findWithDefault :: v -> k -> m v -> v
   member :: k -> m v -> Bool
   null :: m v -> Bool
@@ -51,3 +71,24 @@ class TypeMap m k => TypeMapPartial m k | m -> k where
   unionWithKey :: (k -> v -> v -> v) -> (m v -> m v -> m v)
   unions :: [m v] -> m v
   unionsWithKey :: (k -> v -> v -> v) -> ([m v] -> m v)
+
+-- =
+
+instance (Eq k, Finite k) => TypeMap ((->) k) k where
+  lookup k m = Just (m k)
+  assocs m = Prelude.map (\k -> (k, m k)) enumerate
+  replace k v m = \k' -> if k == k' then v else m k'
+  replaceWith f k m = \k' -> let v = m k' in if k == k' then f v else v
+  map = (.)
+  mapWithKey f m = \k -> f k (m k)
+  mapAccumWithKeyBy ks f = go ks (MS.empty)
+    where go [] m' acc m = (acc, (m' MS.!) . toNatural)
+          go (k:ks) m' acc m =
+            let (acc', v) = f acc k (m k)
+            in go ks (MS.insert (toNatural k) v m') acc' m
+
+instance (Eq k, Finite k) => TypeMapTotal ((->) k) k where
+  build = id
+  get = flip ($)
+
+newtype FnPartial k v = FnPartial { getFn :: k -> Maybe v }
